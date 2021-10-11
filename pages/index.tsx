f63ea82e1a636317292561d518be16d3a6d84557
Daniel Dyrnes <thedanielforum@gmail.com>
2021-10-12 00:14:12 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <main>
-      <a
-        href="/api/auth/login?returnTo=/dashboard"
-        className="w-32 flex items-center justify-center rounded-md bg-black text-white"
-      >
-        Login
-      </a>
-    </main>
-  );
+  return <main></main>;
 };
 
 export default Home;
