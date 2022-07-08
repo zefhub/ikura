0b960cbef13bd12a2efd0999a628a6118547b754
Daniel Dyrnes <thedanielforum@gmail.com>
2022-07-08 14:45:46 +0900

    Added date filtering

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import { Fragment, useState } from "react";
+import { DateTime } from "luxon";
 import { useIntl } from "react-intl";
+import DatePicker from "react-datepicker";
 import { useQuery } from "@apollo/client";
 import toast from "react-hot-toast";
 import { TRANSACTION_AMOUNT_AGGREGATE } from "constants/queries";
@@ -13,8 +15,8 @@ import CategoryChart from "components/CategoryChart";
 
 const Dashboard: NextPage = () => {
   const intl = useIntl();
-  const [dateStart, setDateStart] = useState(new Date());
-  const [dateEnd, setDateEnd] = useState(new Date());
+  const [dateStart, setDateStart] = useState(DateTime.now().startOf("month"));
+  const [dateEnd, setDateEnd] = useState(DateTime.now());
 
   const { data: income, error: incomeError } = useQuery(
     TRANSACTION_AMOUNT_AGGREGATE,
@@ -23,7 +25,7 @@ const Dashboard: NextPage = () => {
         filter: {
           and: [
             { amount: { gt: 0 } },
-            // { date: { between: { min: dateStart, max: dateEnd } } },
+            { date: { between: { min: dateStart, max: dateEnd } } },
           ],
         },
       },
@@ -36,7 +38,14 @@ const Dashboard: NextPage = () => {
   const { data: expense, error: expenseError } = useQuery(
     TRANSACTION_AMOUNT_AGGREGATE,
     {
-      variables: { filter: { and: [{ amount: { lt: 0 } }] } },
+      variables: {
+        filter: {
+          and: [
+            { amount: { lt: 0 } },
+            { date: { between: { min: dateStart, max: dateEnd } } },
+          ],
+        },
+      },
     }
   );
   if (expenseError) {
@@ -47,6 +56,7 @@ const Dashboard: NextPage = () => {
   const { data: transactions, error } = useQuery(GET_TRANSACTIONS, {
     variables: {
       order: { desc: "date" },
+      filter: { date: { between: { min: dateStart, max: dateEnd } } },
       limit: 30,
     },
   });
@@ -78,23 +88,41 @@ const Dashboard: NextPage = () => {
             </div>
           </div>
           <div className="w-1/2 ml-2 pt-2">
-            {!transactions?.queryTransaction.length ? (
-              <p className="w-full mt-2">
-                {intl.formatMessage({ defaultMessage: "No transactions" })}
-              </p>
-            ) : (
-              <Fragment>
-                {transactions.queryTransaction.map((transaction: any) => (
-                  <Transaction
-                    key={transaction.id}
-                    id={transaction.id}
-                    category={transaction.category}
-                    amount={transaction.amount}
-                    date={transaction.date}
-                  />
-                ))}
-              </Fragment>
-            )}
+            <div className="flex flex-row justify-between">
+              {/* @ts-ignore */}
+              <DatePicker
+                selected={dateStart.toJSDate()}
+                onChange={(date: Date) =>
+                  setDateStart(DateTime.fromJSDate(date))
+                }
+                className="mr-2 rounded-md"
+              />
+              {/* @ts-ignore */}
+              <DatePicker
+                selected={dateEnd.toJSDate()}
+                onChange={(date: Date) => setDateEnd(DateTime.fromJSDate(date))}
+                className="rounded-md"
+              />
+            </div>
+            <div>
+              {!transactions?.queryTransaction.length ? (
+                <p className="w-full mt-2">
+                  {intl.formatMessage({ defaultMessage: "No transactions" })}
+                </p>
+              ) : (
+                <Fragment>
+                  {transactions.queryTransaction.map((transaction: any) => (
+                    <Transaction
+                      key={transaction.id}
+                      id={transaction.id}
+                      category={transaction.category}
+                      amount={transaction.amount}
+                      date={transaction.date}
+                    />
+                  ))}
+                </Fragment>
+              )}
+            </div>
           </div>
         </div>
       </div>
